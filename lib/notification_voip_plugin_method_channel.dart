043a90e39e723740685bb8e4dbc98769f78b4575
Raj Shah <raj.shah@cometchat.com>
2025-08-16 02:04:23 +0530

    Update README.md Update CHANGELOG.md Optimized notification_voip_plugin.dart code and added necessary comments.

--- lib/notification_voip_plugin_method_channel.dart
+++ lib/notification_voip_plugin_method_channel.dart
@@ -4,14 +4,17 @@ import 'package:flutter/services.dart';
 import 'notification_voip_plugin_platform_interface.dart';
 
 /// An implementation of [NotificationVoipPluginPlatform] that uses method channels.
-class MethodChannelNotificationVoipPlugin extends NotificationVoipPluginPlatform {
+class MethodChannelNotificationVoipPlugin
+    extends NotificationVoipPluginPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('notification_voip_plugin');
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version = await methodChannel.invokeMethod<String>(
+      'getPlatformVersion',
+    );
     return version;
   }
 }
